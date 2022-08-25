f214bc6596b0f66316eebecfbcb605664fa27221
magalhaesmth <matheusurbano2002@hotmail.com>
2022-08-25 20:04:24 -0300

    Criando crud marca

---
+++ backend/src/main/java/com/dev/backend/service/MarcaService.java
@@ -0,0 +1,37 @@
+package com.dev.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.backend.entity.Marca;
+import com.dev.backend.repository.MarcaRepository;
+
+@Service
+public class MarcaService {
+    
+    @Autowired
+    private MarcaRepository marcaRepository;
+
+    public List<Marca> buscarTodos(){
+        return marcaRepository.findAll();
+    }
+
+    public Marca inserir(Marca marca){
+        marca.setDataCriacao(new Date());
+        Marca marcaNovo = marcaRepository.saveAndFlush(marca);
+        return marcaNovo;
+    }
+
+    public Marca alterar(Marca marca){
+        marca.setDataAtualizacao(new Date());
+        return marcaRepository.saveAndFlush(marca);
+    }
+
+    public void excluir(Long id){
+        Marca marca = marcaRepository.findById(id).get();
+        marcaRepository.delete(marca);
+    }
+}
